765987e2bb0f99e1cc55e6b6230a599fd7508765
Hunger@tharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-01-20 10:45:27 +0530

    Add files via upload

---
+++ mmtometer_GIT.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+      float mm,m;
+      printf("Programming for conversion of mm to meter");
+      printf("\nEnter a value in m : ");
+      scanf("%f",&m);
+      mm = m*1000; 
+      printf("\nThe value of mm : %f",mm);
+
+      
+}
---
+++ mtocm_GIT.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+      float m,cm;
+      printf("programming for find a value of centimeter");
+      printf("Enter a value of meter m : ");
+      scanf("%f",&m);
+      cm = m *100;
+      printf("conversion of m in cm is :%f",cm);
+}
