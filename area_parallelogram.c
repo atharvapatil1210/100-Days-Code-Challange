b0e8fb8aae09c0cb7cb3f6227d2f20bc0e1131ab
Hunger@tharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-01-24 19:49:06 +0530

    Create area_parallelogram.c

---
+++ area_parallelogram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+      float a,h,b;
+      printf("this program for calculation of area of parallelogram");
+      printf("\nEnter a value of base of parallelogram : ");
+      scanf("%f",&b);
+      printf("\nEnter a value of height of parallelogram :");
+      scanf("%f",&h);
+      a =0.5 * b * h ;
+      printf("THE VALUE OF AREA OF PARALELLOGRAM : %f",a);
+}
