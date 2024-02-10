64bcd4e4eae7bb6f50b409d00c03daa5e5f3f8c3
Hunger@tharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-02-11 01:56:35 +0530

    Create num_less.c

---
+++ num_less.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+      int num;
+      printf("\nEnter a number : ");
+      scanf("%d",&num);
+      if(num<100){
+            printf("\nNumber is less than 100");
+      }
+      else{
+            printf("\nNumber is greter than ");
+      }
+}
