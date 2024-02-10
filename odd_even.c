907cc859a2f948da868850e3d987731789493438
Hunger@tharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-02-11 01:59:33 +0530

    Create odd_even.c

---
+++ odd_even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+      int num;
+      printf("\nEnter a number : ");
+      scanf("%d",&num);
+
+      if ( num % 2 == 0 ){
+            printf("\nEven Number ");
+      }
+      else{
+            printf("\nOdd Number");
+      }
+}
