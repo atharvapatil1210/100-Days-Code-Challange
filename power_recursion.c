526080f95aa7b100face34506249d881649063e2
Hunger@tharva <145995913+atharvapatil1210@users.noreply.github.com>
2024-03-26 14:13:05 +0530

    Create power_recursion.c
    C program to calculate the power using recursion
    
    To understand this example, you should have the knowledge of the following C programming topics:
    
        C Functions
        C User-defined functions
        C Recursion

---
+++ power_recursion.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int power(int n , int m );
+int main(){
+  int base , a , result;
+  printf("Enter base numbers : ");
+  scanf("%d",&base);
+  printf("Enter power number(positive integer): ");
+  scanf("%d", &a);
+  result = power(base,a);
+  printf("%d^%d = %d",base,a,result);
+  return 0;
+}
+
+int power(int base , int a){
+  if (a != 0)
+    return (base * power(base, a - 1 ));
+  else 
+    return 1;
+}
+    
